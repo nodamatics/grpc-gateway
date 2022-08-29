29871567c25ad61145f444126c772c480509e4be
Philip Mantrov <philip.mantrov@gmail.com>
2022-08-29 19:23:24 +0300

    move service_url addr to env's

--- main.go
+++ main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"net/http"
+	"os"
 
 	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	pb "grpc-gateway/islamic/pb"
-)
-
-var (
-	// command-line options:
-	// gRPC server endpoint
-	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9106", "gRPC server endpoint")
+	islamic_pb "grpc-gateway/islamic/pb"
 )
 
 func run() error {
+	addr := os.Getenv("SERVICE_URL")
+	if addr == "" {
+		return errors.New("missing env $SERVICE_URL")
+	}
+	grpcServerEndpoint := flag.String("grpc-server-endpoint", os.Getenv("SERVICE_URL"), "gRPC server endpoint")
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -28,7 +29,7 @@ func run() error {
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err := pb.RegisterIslamicProtoHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	err := islamic_pb.RegisterIslamicProtoHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
 	if err != nil {
 		return err
 	}
